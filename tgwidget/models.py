b4a8e8918d4a404ec899c61252142d03aea5b2d9
Andrey Smirnov <mail@ansmirnov.ru>
2018-02-10 10:25:44 +0300

    Update model

--- tgwidget/models.py
+++ tgwidget/models.py
@@ -1,3 +1,7 @@
+import hashlib
+import hmac
+import os
+
 from django.db import models
 
 # Create your models here.
@@ -11,3 +15,17 @@ class TgUser(models.Model):
     tg_photo_url = models.CharField(max_length=200, blank=True, null=True)
     tg_auth_date = models.CharField(max_length=200, blank=True, null=True)
     tg_hash = models.CharField(max_length=200, blank=True, null=True)
+
+    def calc_hash(self):
+        secret_key = hashlib.sha256(os.getenv('BOT_TOKEN').encode('utf-8')).hexdigest()
+        tpl = "auth_date={}\nfirst_name={}\nid={}\nusername={}"
+        data_check_string = tpl.format(
+            self.tg_auth_date.encode('utf-8'),
+            self.tg_first_name.encode('utf-8'),
+            self.tg_id.encode('utf-8'),
+            self.tg_username.encode('utf-8')
+        )
+        return hmac.new(secret_key, data_check_string, hashlib.sha256).hexdigest()
+
+    def check_hash(self):
+        return self.calc_hash() == self.hash
